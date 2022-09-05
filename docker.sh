7802aa5c94fc79787c83eff0ca39e097cbd8fc71
Mudassar000 <111733453+Mudassar000@users.noreply.github.com>
2022-09-05 11:36:11 +0500

    docker.sh
    docker install script

---
+++ docker.sh
@@ -0,0 +1,14 @@
+sudo apt-get remove docker docker-engine docker.io containerd runc -y
+sudo apt-get update
+sudo apt-get install ca-certificates
+sudo apt-get install curl
+sudo apt-get install gnupg
+sudo apt-get install lsb-release
+sudo apt install docker.io -y
+sudo snap install docker
+sudo apt-get update
+sudo apt-get install docker-ce 
+sudo apt-get install docker-ce-cli 
+sudo apt-get install containerd.io 
+sudo apt-get install docker-compose -y
+sudo apt install && sudo apt upgrade
